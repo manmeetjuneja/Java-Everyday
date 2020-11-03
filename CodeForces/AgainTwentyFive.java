abcd4a11c90b9373b3825f7674b51a1ef0a74245
Manmeet Juneja <49336313+manmeetjuneja@users.noreply.github.com>
2020-11-03 20:44:41 +0530

    Add files via upload

---
+++ CodeForces/AgainTwentyFive.java
@@ -0,0 +1,23 @@
+package CodeForces;
+
+import java.util.Scanner;
+
+public class AgainTwentyFive {
+
+	public static void main(String[] args) {
+		long number,sum=0;
+		Scanner sc = new Scanner(System.in);
+		number = sc.nextLong();
+		long answer = (long)Math.pow(5, number);
+		System.out.println(answer);
+//		int length = 2;
+//		for(int i=0;i<length;i++) {
+//			answer = answer%10;
+//			sum = answer *10 + sum;
+//			answer/=10;
+//		}
+//		System.out.println(sum);
+
+	}
+
+}
